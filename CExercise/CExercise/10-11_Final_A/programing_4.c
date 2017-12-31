417c0655ab8d87d1c46df9a18a993661d2f46530
tangyisheng2 <tangyisheng2@sina.com>
2017-12-31 16:08:08 +0800

    10-11
    Create a file for 10-11 final
    bug fix

--- CExercise/CExercise/10-11_Final_A/programing_4.c
+++ CExercise/CExercise/10-11_Final_A/programing_4.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+int fun(char str[], char del);
 int main() {
 
 	char str[80], a;
 	gets_s(str, sizeof(str));
 	scanf_s("%c", &a,1);
+	fun(str, a);
+	puts(str);
+	system("pause");
+	return 0;
+}
+
+int fun(char str[], char del) {
 	int i = 0, j = 0, k = 0;
 	for (i = 0; i < strlen(str); i++) {
 		for (k = 0; k < strlen(str); k++) {
-			if (str[i] == a) {
+			if (str[i] == del) {
 				strcpy_s(str + i, 80 - i, str + i + 1);
 			}
 		}
 	}
-	puts(str);
-
-	system("pause");
-	return 0;
 }
